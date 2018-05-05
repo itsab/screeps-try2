e7af2f89d1f97a1bc4408acc1dc019747f00319b
itsab <sebastian.demmer@gmx.at>
2018-05-05 22:01:06 +0200

    added upgraders

--- dist/main.js
+++ dist/main.js
@@ -1,20 +1,30 @@
 var harvester = require('role.harvester');
 var builder = require('role.builder');
 var guardian = require('role.guardian');
+var upgrader = require('role.upgrader');
 var tower = require('tower');
 
 module.exports.loop = function () {
 
 
 
-    var harvesters = _.filter(Game.creeps, (creep) => {return (creep.memory.role == "harvester")})
+
 
     //spawn harvesters
+    var harvesters = _.filter(Game.creeps, (creep) => {return (creep.memory.role == "harvester")});
     if(harvesters.length < 2) {
         var newName = 'Harvester' + Game.time;
         console.log('Spawning new harvester: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
-            {memory: {role: 'harvester'}});        
+        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
+            {memory: {role: 'harvester'}});
+    }
+	//spawn upgrader
+    var upgraders = _.filter(Game.creeps, (creep) => {return (creep.memory.role == "upgrader")});
+    if(upgraders.length < 2) {
+        var newName = 'Upgrader' + Game.time;
+        console.log('Spawning new upgrader: ' + newName);
+        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
+            {memory: {role: 'upgrader'}});
     }
     
     //use creeps
