8bb9e9360628b0427920f6db9472f5668a75aecf
itsab <sebastian.demmer@gmx.at>
2018-05-10 20:14:38 +0200

    added common function

--- dist/role.driller.js
+++ dist/role.driller.js
@@ -4,13 +4,14 @@ var roleDriller = {
     run: function(creep) {
 
         //mine if already near to energy source
-        creep.harvest(creep.pos.findInRange(FIND_SOURCES,1)[0]);
+        var source = creep.pos.findInRange(FIND_SOURCES,1)[0];
+        if(source) creep.harvest(creep.pos.findInRange(FIND_SOURCES,1)[0]);
 
         //search for containers near sources with no creeps on it
         var containersNearSource = creep.room.find(FIND_STRUCTURES, {
             filter: (structure) => {
-                return (structure.structureType == STRUCTURE_CONTAINER &&
-                    structure.pos.findInRange(FIND_SOURCES,1).length > 0 &&
+                return (structure.structureType == STRUCTURE_CONTAINER) &&
+                    (structure.pos.findInRange(FIND_SOURCES,1).length > 0) &&
                     structure.pos.lookFor(FIND_CREEPS).length == 0)
             }});
 
