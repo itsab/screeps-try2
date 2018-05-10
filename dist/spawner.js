3d8f6db1d987b67502e60b1f29dcc1e1b83383bd
itsab <sebastian.demmer@gmx.at>
2018-05-10 14:59:39 +0200

    added spawner

--- dist/spawner.js
+++ dist/spawner.js
@@ -4,32 +4,28 @@ var spawner = {
         var numberCreeps = spawnerObj.numberCreeps;
 
         //check if there is a creep already waiting to spawn
-        if(spawnerObj.toSpawn == false)
+
+        //loop through all roles
+        for(var key in numberCreeps)
         {
-            console.log("false");
-            //loop through all roles
-            for(var key in numberCreeps)
+            var numberOfAliveCreeps = _.filter(Game.creeps, (creep) => {return (creep.memory.role == key)});
+            if(numberOfAliveCreeps < numberCreeps[key])
             {
-                var numberOfAliveCreeps = _.filter(Game.creeps, (creep) => {return (creep.memory.role == key)});
-                if(numberOfAliveCreeps < numberCreeps[key])
-                {
-                    console.log(numberOfAliveCreeps + "<" + numberCreeps[key]);
-                    spawnerObj.toSpawn = true;
+                console.log(numberOfAliveCreeps + "<" + numberCreeps[key]);
+                spawnerObj.toSpawn = true;
 
-                    //creating new creeps name
-                    var newName = key + Game.time;
+                //creating new creeps name
+                var newName = key + Game.time;
 
 
-                    //spawning new creep with the role already in memory
-                    if(Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,{memory: {role: key}}) == OK){
-                        spawnerObj.toSpawn = false;
-                        console.log('Spawning new '+ key+ ': ' + newName);
-                    };
-                }
+                //spawning new creep with the role already in memory
+                if(Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,{memory: {role: key}}) == OK){
+                    spawnerObj.toSpawn = false;
+                    console.log('Spawning new '+ key+ ': ' + newName);
+                };
             }
-        } else {
-
         }
+
         Memory.spawner = spawnerObj;
     }
 }
