a8b557254a4939d1d23d96be42d9a28293b9ce34
itsab <sebastian.demmer@gmx.at>
2018-05-10 14:22:33 +0200

    added builders

--- dist/common.js
+++ dist/common.js
@@ -1,6 +1,6 @@
 var common = {
     getEnergyFromSpawn: function (creep) {
-        if (Memory.spawer.toSpawn == false) {
+        if (Memory.spawner.toSpawn == false) {
             if (creep.withdraw(creep.room.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(Game.spawns.Spawn1);
             }
