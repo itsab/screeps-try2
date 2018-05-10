e48f1440a182ac007a256a143c5d1f92741ea0c9
itsab <sebastian.demmer@gmx.at>
2018-05-10 19:26:56 +0200

    added common function

--- dist/common.js
+++ dist/common.js
@@ -7,6 +7,36 @@ var common = {
         } else {
             creep.moveTo(Game.spawns.Spawn1,{visualizePathStyle: {stroke: '#ffaa00'}});
         }
+    },
+
+    collectEnergyFromContainers: function (creep) {
+        var droppedEnergy = creep.pos.findInRange(FIND_DROPPED_RESOURCES, 10, {filter: {resourceType: RESOURCE_ENERGY}});
+        if(droppedEnergy.length > 0)
+        {
+            if(creep.pickup(droppedEnergy[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(droppedEnergy[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        } else
+        {
+            var containers = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_CONTAINER) &&
+                        structure.energy / structure.energyCapacity > 0.1;
+                }
+            });
+            if(containers.length > 0)
+            {
+                if(creep.withdraw(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(containers[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                }
+            }
+
+        }
+
+        var nearSource = creep.pos.findClosestByPath(FIND_SOURCES);
+        if(creep.harvest(nearSource) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(nearSource, {visualizePathStyle: {stroke: '#ffaa00'}});
+        }
     }
 }
 
