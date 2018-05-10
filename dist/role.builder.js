50ef37f0ed89bd9912431d724ef0550c6f06ce5d
itsab <sebastian.demmer@gmx.at>
2018-05-10 16:25:52 +0200

    added driller

--- dist/role.builder.js
+++ dist/role.builder.js
@@ -4,8 +4,8 @@ var roleBuilder = {
 	run: function(creep) {
         if(creep.carry.energy == 0) {
 
-            //check if the nearest source is closer than spawn
-            if(creep.pos.getRangeTo(creep.pos.findClosestByPath(FIND_SOURCES)) < creep.pos.getRangeTo(creep.pos.findClosestByPath(FIND_MY_SPAWNS)))
+            //check if the nearest source is closer than spawn or toSpawn is true
+            if(creep.pos.getRangeTo(creep.pos.findClosestByPath(FIND_SOURCES)) < creep.pos.getRangeTo(creep.pos.findClosestByPath(FIND_MY_SPAWNS)) || Memory.spawner.toSpawn == true)
             {
                 var nearSource = creep.pos.findClosestByPath(FIND_SOURCES);
                 if(creep.harvest(nearSource) == ERR_NOT_IN_RANGE) {
