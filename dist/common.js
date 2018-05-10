d15b5007d405d22f81778bfd7dc35ab593b1b586
itsab <sebastian.demmer@gmx.at>
2018-05-10 17:26:11 +0200

    updated builder

--- dist/common.js
+++ dist/common.js
@@ -2,10 +2,10 @@ var common = {
     getEnergyFromSpawn: function (creep) {
         if (Memory.spawner.toSpawn == false) {
             if (creep.withdraw(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns.Spawn1);
+                creep.moveTo(Game.spawns.Spawn1,{visualizePathStyle: {stroke: '#ffaa00'}});
             }
         } else {
-            creep.moveTo(Game.spawns.Spawn1);
+            creep.moveTo(Game.spawns.Spawn1,{visualizePathStyle: {stroke: '#ffaa00'}});
         }
     }
 }
