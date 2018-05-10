2a414fb59bd1214938c6ab758d3c0a3077f57125
itsab <sebastian.demmer@gmx.at>
2018-05-10 16:47:29 +0200

    updated builder

--- dist/role.builder.js
+++ dist/role.builder.js
@@ -2,10 +2,24 @@ var common = require('common');
 
 var roleBuilder = {
 	run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
+        if(creep.memory.building && creep.carry.energy == 0) {
+            creep.memory.building = false;
+            creep.say('harvest');
+        }
+        if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.building = true;
+            creep.say('building');
+        }
 
-            //check if the nearest source is closer than spawn or toSpawn is true
-            if(creep.pos.getRangeTo(creep.pos.findClosestByPath(FIND_SOURCES)) < creep.pos.getRangeTo(creep.pos.findClosestByPath(FIND_MY_SPAWNS)) || Memory.spawner.toSpawn == true)
+        if(creep.memory.building) {
+            var target = creep.pos.findClosestByPath(FIND_MY_CONSTRUCTION_SITES);
+            if(creep.build(target) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target);
+            }
+        }
+        else {
+            if(creep.pos.getRangeTo(creep.pos.findClosestByPath(FIND_SOURCES)) < creep.pos.getRangeTo(creep.pos.findClosestByPath(FIND_MY_SPAWNS))
+                || Memory.spawner.toSpawn == true)
             {
                 var nearSource = creep.pos.findClosestByPath(FIND_SOURCES);
                 if(creep.harvest(nearSource) == ERR_NOT_IN_RANGE) {
@@ -14,14 +28,10 @@ var roleBuilder = {
             } else {
                 common.getEnergyFromSpawn(creep);
             }
+        }
 
-        }
-        else {
-            var target = creep.pos.findClosestByPath(FIND_MY_CONSTRUCTION_SITES);
-            if(creep.build(target) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(target);
-            }
-        }
+
+
 	}
 };
 module.exports = roleBuilder;
