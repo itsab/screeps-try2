18f8031406c6a13f1dfbf4eb9b2d2abf437e1941
itsab <sebastian.demmer@gmx.at>
2018-05-10 19:12:46 +0200

    added repair feature

--- dist/role.driller.js
+++ dist/role.driller.js
@@ -7,8 +7,7 @@ var roleDriller = {
         var containersNearSource = creep.room.find(FIND_STRUCTURES, {
             filter: (structure) => {
                 return (structure.structureType == STRUCTURE_CONTAINER &&
-                    structure.pos.findInRange(FIND_SOURCES,1).length > 0 &&
-                    structure.pos.lookFor(FIND_CREEPS).length == 0)
+                    structure.pos.findInRange(FIND_SOURCES,1).length > 0)
             }});
 
 
