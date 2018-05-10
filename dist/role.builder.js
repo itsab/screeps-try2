2fde5701d50919a2154c41b9c5ca032e08c26912
itsab <sebastian.demmer@gmx.at>
2018-05-10 17:36:50 +0200

    updated builder

--- dist/role.builder.js
+++ dist/role.builder.js
@@ -21,10 +21,10 @@ var roleBuilder = {
             //checks if sources are closer than spawn
             var sourceCloserthanSpawn = creep.pos.getRangeTo(creep.pos.findClosestByPath(FIND_SOURCES)) < creep.pos.getRangeTo(creep.pos.findClosestByPath(FIND_MY_SPAWNS));
             //forces builders to take spawn energy if spawn is full
-            var Spawn1full = Game.spawns["Spawn1"].energyCapacity == Game.spawns["Spawn1"].energy && Game.spawns["Spawn1"].energy > 0;
-
-
-            if( (sourceCloserthanSpawn || Memory.spawner.toSpawn == true) && (!Spawn1full)
+            var Spawn1full = Game.spawns["Spawn1"].energyCapacity == Game.spawns["Spawn1"].energy;
+            //force builders get sources when spawn1 is empty
+            var Spawn1empty = Game.spawns["Spawn1"].energy > 0;
+            if( (sourceCloserthanSpawn || Memory.spawner.toSpawn == true) && (!Spawn1full || Spawn1empty))
             {
                 var nearSource = creep.pos.findClosestByPath(FIND_SOURCES);
                 if(creep.harvest(nearSource) == ERR_NOT_IN_RANGE) {
