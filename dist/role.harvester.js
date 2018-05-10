f0b30044ee07acd7076a5eec9108e480d56c3559
itsab <sebastian.demmer@gmx.at>
2018-05-10 18:22:45 +0200

    updated upgrader

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -9,6 +9,15 @@ var roleHarvester = {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }*/
 
+	        //pickup dropped energy in range
+            var droppedEnergy = creep.pos.findInRange(FIND_DROPPED_ENERGY, 10);
+	        if(droppedEnergy.length > 0)
+            {
+                if(creep.pickup(droppedEnergy[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(droppedEnergy[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                }
+            }
+
             var nearSource = creep.pos.findClosestByPath(FIND_SOURCES);
             if(creep.harvest(nearSource) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(nearSource, {visualizePathStyle: {stroke: '#ffaa00'}});
