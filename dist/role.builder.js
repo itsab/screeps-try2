f665f97e5014fb2984b6313d65d1e3907f43d4c0
itsab <sebastian.demmer@gmx.at>
2018-05-10 16:27:09 +0200

    updated builder

--- dist/role.builder.js
+++ dist/role.builder.js
@@ -17,10 +17,10 @@ var roleBuilder = {
 
         }
         else {
-            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
+            var target = creep.room.findClosestByPath(FIND_CONSTRUCTION_SITES);
+            if(target.length) {
+                if(creep.build(target) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
                 }
             }
         }
