049dcb5b1b316d74138231218bcc295b89266d6c
itsab <sebastian.demmer@gmx.at>
2018-05-10 18:57:59 +0200

    added repair feature

--- dist/role.driller.js
+++ dist/role.driller.js
@@ -12,7 +12,7 @@ var roleDriller = {
             }});
 
 
-        console.log(containersNearSource);
+        console.log("containers near sources with no creep on it: "+containersNearSource);
         if(containersNearSource.length > 0)
         {
             if(creep.harvest(containersNearSource[0].pos.findInRange(FIND_SOURCES,1)) == ERR_NOT_IN_RANGE) {
