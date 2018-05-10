cb922a0da01545de11d2a6d84a80dba4fbdf6a07
itsab <sebastian.demmer@gmx.at>
2018-05-10 16:16:24 +0200

    added driller

--- dist/role.builder.js
+++ dist/role.builder.js
@@ -3,6 +3,17 @@ var common = require('common');
 var roleBuilder = {
 	run: function(creep) {
         if(creep.carry.energy == 0) {
+
+            //check if the nearest source is closer than spawn
+            if(creep.pos.getRangeTo(creep.pos.findClosestByPath(FIND_SOURCES)) < creep.pos.getRangeTo(creep.pos.findClosestByPath(FIND_MY_SPAWNS)))
+            {
+                var nearSource = creep.pos.findClosestByPath(FIND_SOURCES);
+                if(creep.harvest(nearSource) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(nearSource, {visualizePathStyle: {stroke: '#ffaa00'}});
+                }
+            } else {
+
+            }
             common.getEnergyFromSpawn(creep);
         }
         else {
---
+++ dist/role.driller.js
@@ -0,0 +1,25 @@
+var roleDriller = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+
+        //search for containers near sources with no creeps on it
+        var containersNearSource = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_CONTAINER) &&
+                    structure.pos.findInRange(FIND_SOURCES,1).length > 0 &&
+                    structure.pos.lookFor(FIND_CREEPS).length > 0
+            }});
+
+
+        console.log(containersNearSource);
+        if(containersNearSource.length > 0)
+        {
+            if(creep.harvest(containersNearSource[0].pos.findInRange(FIND_SOURCES,1)) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(containersNearSource[0], {visualizePathStyle: {stroke: '#ffcc00'}});
+            }
+        }
+   }
+}
+
+module.exports = roleDriller;
