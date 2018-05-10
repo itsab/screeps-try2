a011da26119b89a9452fa5bb203439174002b7e0
itsab <sebastian.demmer@gmx.at>
2018-05-10 20:15:18 +0200

    added common function

--- dist/role.driller.js
+++ dist/role.driller.js
@@ -5,22 +5,28 @@ var roleDriller = {
 
         //mine if already near to energy source
         var source = creep.pos.findInRange(FIND_SOURCES,1)[0];
-        if(source) creep.harvest(creep.pos.findInRange(FIND_SOURCES,1)[0]);
-
-        //search for containers near sources with no creeps on it
-        var containersNearSource = creep.room.find(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_CONTAINER) &&
-                    (structure.pos.findInRange(FIND_SOURCES,1).length > 0) &&
-                    structure.pos.lookFor(FIND_CREEPS).length == 0)
+        if(source){
+            creep.harvest(creep.pos.findInRange(FIND_SOURCES,1)[0]);
+        } else
+        {
+            var containersNearSource = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_CONTAINER) &&
+                        (structure.pos.findInRange(FIND_SOURCES,1).length > 0) &&
+                        structure.pos.lookFor(FIND_CREEPS).length == 0)
             }});
 
 
-        console.log("containers near sources with no creep on it: "+containersNearSource);
-        if(containersNearSource.length > 0)
-        {
-            creep.moveTo(containersNearSource[0], {visualizePathStyle: {stroke: '#ffcc00'}});
+            console.log("containers near sources with no creep on it: "+containersNearSource);
+            if(containersNearSource.length > 0)
+            {
+                creep.moveTo(containersNearSource[0], {visualizePathStyle: {stroke: '#ffcc00'}});
+            }
         }
+
+
+        //search for containers near sources with no creeps on it
+
    }
 }
 
