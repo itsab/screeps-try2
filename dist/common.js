4825c568871301755f0dee5a8bef467700f80d8b
itsab <sebastian.demmer@gmx.at>
2018-05-10 14:21:51 +0200

    added builders

--- dist/common.js
+++ dist/common.js
@@ -1,12 +1,11 @@
 var common = {
-    getEnergyFromSpawn: function(creep) {
-    if(Memory.spawer.toSpawn == false)
-    {
-        if(creep.withdraw(creep.room.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(Game.spawns.Spawn1);
+    getEnergyFromSpawn: function (creep) {
+        if (Memory.spawer.toSpawn == false) {
+            if (creep.withdraw(creep.room.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.spawns.Spawn1);
+            }
         }
     }
-};
-
 }
+
 module.exports = common;
