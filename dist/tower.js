106c54cc6e91e805686c67b1495291ea0859779c
itsab <sebastian.demmer@gmx.at>
2018-05-11 17:30:53 +0200

    updated towers

--- dist/tower.js
+++ dist/tower.js
@@ -1,19 +1,27 @@
 var tower = {
-    run: function(){
-        //attack
+    run: function() {
+        var towers = Memory.towers.map(Game.getObjectById);
 
+        for(var key in towers)
+        {
+            var tower = towers[key];
+            //attack
+            //TODO attack logic (range restricted)
 
-        //repair structures
-        var repairTargets = creep.room.find(FIND_STRUCTURES, {
-            filter: (structure) => { var calc = structure.hits / structure.hitsMax; return calc < 0.5; }
-        });
+            //repair structures
+            var repairTargets = tower.room.find(FIND_STRUCTURES, {
+                filter: (structure) => { var calc = structure.hits / structure.hitsMax; return calc < 0.7; }
+            });
 
-        if(repairTargets.length > 0) {
-            var repairTarget = creep.pos.findClosestByPath(repairTargets);
-            if(creep.repair(repairTarget) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(repairTarget);
+            if(repairTargets.length > 0) {
+                var repairTarget = tower.pos.findClosestByRange(repairTargets);
+                if(tower.repair(repairTarget) == ERR_NOT_IN_RANGE) {
+                    tower.moveTo(repairTarget);
+                }
             }
         }
+
+
     }
 }
 
