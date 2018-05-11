84dd0fd4b17cb15001c8f9b493669276d195289d
itsab <sebastian.demmer@gmx.at>
2018-05-11 17:35:25 +0200

    updated towers

--- dist/tower.js
+++ dist/tower.js
@@ -9,16 +9,20 @@ var tower = {
             //TODO attack logic (range restricted)
 
             //repair structures
-            var repairTargets = tower.room.find(FIND_STRUCTURES, {
-                filter: (structure) => { var calc = structure.hits / structure.hitsMax; return calc < 0.7; }
-            });
+            if(tower.energy / tower.energyCapacity > 0.5)
+            {
+                var repairTargets = tower.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => { var calc = structure.hits / structure.hitsMax; return calc < 0.7; }
+                });
 
-            if(repairTargets.length > 0) {
-                var repairTarget = tower.pos.findClosestByRange(repairTargets);
-                if(tower.repair(repairTarget) == ERR_NOT_IN_RANGE) {
-                    tower.moveTo(repairTarget);
+                if(repairTargets.length > 0) {
+                    var repairTarget = tower.pos.findClosestByRange(repairTargets);
+                    if(tower.repair(repairTarget) == ERR_NOT_IN_RANGE) {
+                        tower.moveTo(repairTarget);
+                    }
                 }
             }
+
         }
 
 
