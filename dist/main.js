b0c61724627417aba6dac613d0329cda391694e8
itsab <sebastian.demmer@gmx.at>
2018-05-12 18:41:58 +0200

    updated every10ticks

--- dist/main.js
+++ dist/main.js
@@ -126,6 +126,8 @@ var every10ticks = function() {
     {
         var room = Game.rooms[roomName];
         console.log(room);
+        console.log(room.find(FIND_STRUCTURES, {filter:{structureType: STRUCTURE_TOWER}}).map(function(element){return element.id}));
+
         towers = towers_.concat(room.find(FIND_STRUCTURES, {filter:{structureType: STRUCTURE_TOWER}}).map(function(element){return element.id}));
         containers = containers_.concat(room.find(FIND_STRUCTURES, {filter:{structureType: STRUCTURE_TOWER}}).map(function(element){return element.id}));
     }
