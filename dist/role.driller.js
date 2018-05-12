c96bb0e17c5b282693af6757db241d4ec9417cea
itsab <sebastian.demmer@gmx.at>
2018-05-12 22:30:09 +0200

    updated driller containers

--- dist/role.driller.js
+++ dist/role.driller.js
@@ -3,40 +3,52 @@ var roleDriller = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
-        //mine if already near to energy source
-        var source = creep.pos.findInRange(FIND_SOURCES,1)[0];
-        if(source){
-            creep.harvest(creep.pos.findInRange(FIND_SOURCES,1)[0]);
-        } else
+        //change room according to creep.memory.roomName
+        if(creep.pos.roomName != creep.memory.roomName && creep.memory.roomName != undefined)
         {
-            var containers = Memory.containers.map(Game.getObjectById);
+            const route = Game.map.findRoute(creep.room, creep.memory.roomName);
+            if(route.length > 0) {
+                creep.say(route[0].room);
+                const exit = creep.pos.findClosestByRange(route[0].exit);
+                creep.moveTo(exit,{visualizePathStyle: {stroke: '#ffffff'}});
+            }
+        } else {
 
-            var containersNearSource = creep.pos.findClosestByRange(containers, {
-                filter: (structure) => {
-                    return structure.structureType == STRUCTURE_CONTAINER &&
-                        structure.pos.findInRange(FIND_SOURCES, 1).length > 0 &&
-                        (structure.pos.lookFor(LOOK_CREEPS).length == 0 || structure.pos.lookFor(LOOK_CREEPS)[0].memory.role != "driller");
-                }});
-
-
-            console.log("containers near sources with no creep on it: "+containersNearSource);
-            if(containersNearSource)
+            //mine if already near to energy source
+            var source = creep.pos.findInRange(FIND_SOURCES,1)[0];
+            if(source){
+                creep.harvest(creep.pos.findInRange(FIND_SOURCES,1)[0]);
+            } else
             {
+                var containers = Memory.containers.map(Game.getObjectById);
 
-                if(creep.pos.getRangeTo(containersNearSource) > 1){
-                    //move to empty container spot
-                    creep.moveTo(containersNearSource, {visualizePathStyle: {stroke: '#ffcc00'}});
-                } else {
-                    //move to not empty container spot and push creep away
-                    var otherCreep = containersNearSource.pos.lookFor(LOOK_CREEPS)[0];
+                var containersNearSource = creep.pos.findClosestByRange(containers, {
+                    filter: (structure) => {
+                        return structure.structureType == STRUCTURE_CONTAINER &&
+                            structure.pos.findInRange(FIND_SOURCES, 1).length > 0 &&
+                            (structure.pos.lookFor(LOOK_CREEPS).length == 0 || structure.pos.lookFor(LOOK_CREEPS)[0].memory.role != "driller");
+                    }});
 
-                    //move otherCreep out of the way (direction of room controller)
-                    otherCreep.moveTo(otherCreep.room.controller);
-                    creep.moveTo(containersNearSource, {visualizePathStyle: {stroke: '#ffcc00'}})
+
+                console.log("containers near sources with no creep on it: "+containersNearSource);
+                if(containersNearSource)
+                {
+
+                    if(creep.pos.getRangeTo(containersNearSource) > 1){
+                        //move to empty container spot
+                        creep.moveTo(containersNearSource, {visualizePathStyle: {stroke: '#ffcc00'}});
+                    } else {
+                        //move to not empty container spot and push creep away
+                        var otherCreep = containersNearSource.pos.lookFor(LOOK_CREEPS)[0];
+
+                        //move otherCreep out of the way (direction of room controller)
+                        otherCreep.moveTo(otherCreep.room.controller);
+                        creep.moveTo(containersNearSource, {visualizePathStyle: {stroke: '#ffcc00'}})
+                    }
+
+
+
                 }
-
-
-
             }
         }
 
