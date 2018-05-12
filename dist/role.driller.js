b72d9f2c444ec4afb9c63d692504329bce4ec1c5
itsab <sebastian.demmer@gmx.at>
2018-05-12 21:46:25 +0200

    updated respawner

--- dist/role.driller.js
+++ dist/role.driller.js
@@ -9,18 +9,25 @@ var roleDriller = {
             creep.harvest(creep.pos.findInRange(FIND_SOURCES,1)[0]);
         } else
         {
-            var containersNearSource = creep.room.find(FIND_STRUCTURES, {
+            var containersNearSource = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return structure.structureType == STRUCTURE_CONTAINER &&
                         structure.pos.findInRange(FIND_SOURCES, 1).length > 0 &&
-                        structure.pos.lookFor(LOOK_CREEPS).length == 0;
+                        (structure.pos.lookFor(LOOK_CREEPS).length == 0 || structure.pos.lookFor(LOOK_CREEPS)[0].memory.role != "driller");
                 }});
 
 
-            //console.log("containers near sources with no creep on it: "+containersNearSource);
-            if(containersNearSource.length > 0)
+            console.log("containers near sources with no creep on it: "+containersNearSource);
+            if(containersNearSource)
             {
-                creep.moveTo(containersNearSource[0], {visualizePathStyle: {stroke: '#ffcc00'}});
+                var asd;
+                if(asd){
+
+                }
+                //move to empty container spot
+                creep.moveTo(containersNearSource, {visualizePathStyle: {stroke: '#ffcc00'}})
+
+                //move to not empty container spot and push creep away
             }
         }
 
