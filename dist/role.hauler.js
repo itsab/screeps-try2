77e85b2fd893385cf8cb6c5f6fcb8ebae1cdd37a
itsab <sebastian.demmer@gmx.at>
2018-05-12 19:42:20 +0200

    updated hauler

--- dist/role.hauler.js
+++ dist/role.hauler.js
@@ -5,8 +5,10 @@ var roleHauler = {
     /** @param {Creep} creep **/
     run: function(creep) {
         var task = creep.memory.task;
+        console.log("hauler running");
         if(task != undefined)
         {
+            console.log("task defined");
             common.runTask(creep,task);
         } else {
             creep.say("no task");
