7842c6b1bf9aa623f42865517cc67e99f63aa7e3
itsab <sebastian.demmer@gmx.at>
2018-05-13 19:49:17 +0200

    updated energy pickup

--- dist/role.hauler.js
+++ dist/role.hauler.js
@@ -8,7 +8,7 @@ var roleHauler = {
         if(task != undefined)
         {
             if(common.runTask(creep,task) == ERR_NOT_ENOUGH_RESOURCES){
-
+                common.getEnergyFromFloor(creep,10);
             }
         } else {
             creep.say("no task");
