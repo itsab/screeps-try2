41a98c2e3e6d0a99f784afbe5c735ce9c631bf68
itsab <sebastian.demmer@gmx.at>
2018-05-13 19:27:18 +0200

    updated energy grab to pick up energy from floor

--- dist/common.js
+++ dist/common.js
@@ -30,7 +30,7 @@ var common = {
         });
 
         //find closest dropped energy in range
-        var dropped = creep.pos.findInRange(FIND_DROPPED_RESOURCES,{filter:{}});
+        var dropped = creep.pos.findInRange(FIND_DROPPED_RESOURCES,{filter:{resourceType:RESOURCE_ENERGY}});
         if(dropped.length > 0) {
             console.log("DEBUG:dropped");
             var energy = creep.pos.findClosestByPath(dropped);
