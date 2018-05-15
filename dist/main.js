fb17956d04dff00c69c5672b0b67e3970f2cb194
itsab <sebastian.demmer@gmx.at>
2018-05-15 20:00:39 +0200

    added hauler again

--- dist/main.js
+++ dist/main.js
@@ -16,7 +16,7 @@ Memory.spawner =   {
         harvester: { body:[MOVE,MOVE,CARRY,CARRY,WORK], buildCount: 2, priority:10, maxModifier:3 },
         driller: { body:[MOVE,MOVE,WORK,WORK], buildCount: 3, priority:8, maxModifier:2 },
         builder: { body:[MOVE,MOVE,CARRY,CARRY,WORK], buildCount: 3, priority:3, maxModifier:3 },
-        hauler: { body:[MOVE,MOVE,CARRY,CARRY,CARRY,CARRY], buildCount: 0, priority:6, maxModifier:3 },
+        hauler: { body:[MOVE,MOVE,CARRY,CARRY,CARRY,CARRY], buildCount: 2, priority:6, maxModifier:3 },
         upgrader: { body:[MOVE,MOVE,CARRY,CARRY,WORK], buildCount: 3, priority:2, maxModifier:3 },
         guard: { body:[MOVE,MOVE,ATTACK,ATTACK], buildCount: 1, priority:4, maxModifier:2 },
 		claimer: { body:[MOVE,CLAIM], buildCount: 2, priority:1, maxModifier:2 }
