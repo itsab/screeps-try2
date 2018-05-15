95fb0458e001ec95f4294728a0296411aa5ef35c
itsab <sebastian.demmer@gmx.at>
2018-05-15 23:00:40 +0200

    updated guards

--- dist/main.js
+++ dist/main.js
@@ -18,7 +18,7 @@ Memory.spawner =   {
         builder: { body:[MOVE,MOVE,CARRY,CARRY,WORK], buildCount: 2, priority:3, maxModifier:3 },
         hauler: { body:[MOVE,MOVE,CARRY,CARRY,CARRY,CARRY], buildCount: 4, priority:6, maxModifier:4 },
         upgrader: { body:[MOVE,MOVE,CARRY,CARRY,WORK], buildCount: 3, priority:2, maxModifier:3 },
-        guard: { body:[MOVE,MOVE,ATTACK,ATTACK], buildCount: 1, priority:4, maxModifier:2 },
+        guard: { body:[MOVE,MOVE,ATTACK,ATTACK], buildCount: 2, priority:4, maxModifier:2 },
 		claimer: { body:[MOVE,CLAIM], buildCount: 2, priority:1, maxModifier:2 }
     },
     //spawnQueue: []
