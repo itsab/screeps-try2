264aed31c8c394129deaaafb836a5347cd7ab2cd
itsab <sebastian.demmer@gmx.at>
2018-05-15 19:17:55 +0200

    updated guardians

--- dist/role.guardian.js
+++ dist/role.guardian.js
@@ -1,11 +1,19 @@
 var roleGuardian = {
     run: function(creep) {
     	var targets = creep.room.find(FIND_HOSTILE_CREEPS);
-    	if(targets.length) {
+    	var target = creep.pos.findClosestByPath(targets);
+    	if(target) {
     		if(creep.attack(targets[0]) == ERR_NOT_IN_RANGE) {
     			creep.moveTo(targets[0]);		
     		}
     	}
+
+        var target = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES);
+        if(target) {
+            if(creep.attack(targets[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets[0]);
+            }
+        }
     }
 }
 module.exports = roleGuardian;
