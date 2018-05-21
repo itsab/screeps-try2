7b87b17392dcc045873e865466b71bfe34eed1a7
itsab <sebastian.demmer@gmx.at>
2018-05-21 15:37:02 +0200

    updated upgraders

--- dist/role.upgrader.js
+++ dist/role.upgrader.js
@@ -25,7 +25,7 @@ var roleUpgrader = {
 
             if(creep.memory.upgrading) {
                 if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                    var path = PathFinder.search(creep.pos, creep.room.controller);
+                    var path = creep.pos.findPathTo(creep.room.controller);
                     console.log(JSON.stringify(path));
                     creep.moveByPath(path);
                 }
